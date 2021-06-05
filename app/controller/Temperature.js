022c5e8b68da39ef9403d58ae432b2cf86a93ab5
Dhingra Somya <som.dhingra@yahoo.in>
2021-06-05 20:28:10 +0800

    fix: return HTTP error code 400 for incorrect requests made
    fixes #2

--- app/controller/Temperature.js
+++ app/controller/Temperature.js
@@ -7,11 +7,11 @@ export const TemperatureConvert = (req, res) => {
 	let inspected_value = !isNaN(parseFloat(value)) && !isNaN(value - 0) ? value : null
 	precision = !isNaN(parseFloat(precision)) && !isNaN(precision - 0) ? precision : 2
 
-	if (!inspected_value) return res.status(404).send({ error: 'INVALID VALUE', data: { value: value } })
+	if (!inspected_value) return res.status(400).send({ error: 'INVALID VALUE', data: { value: value } })
 
 	let result = convert(inspected_value, unit, precision)
 
-	if (result.code) return res.status(404).send({ error: result.code, data: result.data })
+	if (result.code) return res.status(400).send({ error: result.code, data: result.data })
 
 	return res.send(result)
 }
